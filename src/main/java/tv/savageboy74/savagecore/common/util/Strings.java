f256a41aac93ce3c5596b3f9ab86d0ae50ead288
woody104 <woody104@gmail.com>
2015-02-23 09:27:23 -0500

    Added ItemHelper

--- src/main/java/tv/savageboy74/savagecore/common/util/Strings.java
+++ src/main/java/tv/savageboy74/savagecore/common/util/Strings.java
@@ -30,4 +30,25 @@ public class Strings
         public static final String CONFIG_UPDATES_NAME = "Check for Updates";
         public static final String CONFIG_UPDATES_DESC = "Allow SavageCore to check for updates.";
     }
+
+    public static final class NBT
+    {
+        public static final String ITEMS = "Items";
+        public static final String LOOT_BAG_GUI_OPEN = "lootBagGuiOpen";
+        public static final String FERTILIZER_GUI_OPEN = "fertilizerGuiOpen";
+        public static final String UUID_MOST_SIG = "UUIDMostSig";
+        public static final String UUID_LEAST_SIG = "UUIDLeastSig";
+        public static final String OWNER = "owner";
+        public static final String OWNER_UUID_MOST_SIG = "ownerUUIDMostSig";
+        public static final String OWNER_UUID_LEAST_SIG = "ownerUUIDLeastSig";
+        public static final String CUSTOM_NAME = "CustomName";
+        public static final String DIRECTION = "teDirection";
+        public static final String STATE = "teState";
+        public static final String CHRISTMAS_GIFT_DELIVERED = "givenGift";
+    }
+
+    public static final class Messages
+    {
+        public static final String NO_OWNER = "tooltip.savagecore:none";
+    }
 }
